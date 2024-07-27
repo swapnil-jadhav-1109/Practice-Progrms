b83b743a27afd58eccfb31333594ffa96b033d12
swapnil-jadhav-1109 <swapniljadhav4912@gmail.com>
2024-07-27 14:48:46 +0530

    Unique Array

---
+++ uniqArray.js
@@ -0,0 +1,19 @@
+
+let arr = [1,2,3,4,2,3,6,7];
+function Unique(arr){
+ let uniqArry = [];
+ for(i=0;i<arr.length;i++){
+    let uniq = true;
+  for(j=0;j<uniqArry.length;j++){
+    if(arr[i] === uniqArry[j]){
+        uniq = false;
+        break;
+    }
+  }
+  if(uniq){
+    uniqArry.push(arr[i]);
+  }
+}
+    return uniqArry;
+}
+console.log(Unique(arr));
